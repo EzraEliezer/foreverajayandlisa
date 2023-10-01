58cb114d33f112abca761030efe77e07d7ebc0f4
Ebenezer <58320436+thebenezer@users.noreply.github.com>
2023-10-02 00:43:30 +0530

    updates

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ import './style.css'
         document.getElementById("days").innerText = Math.floor(distance / (day));
         document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
         document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
-        // document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
         //do something later when date is reached
         if (distance < 0) {
